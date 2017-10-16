c7fc07da0300fdc163d15080ab46d2ee6d1abf98
Josh Day <emailjoshday@gmail.com>
2017-10-16 15:49:23 -0400

    getting started

--- src/LearningStrategies.jl
+++ src/LearningStrategies.jl
@@ -1,45 +1,294 @@
 __precompile__(true)
-
 module LearningStrategies
 
-# ----------------------------------------------------------------------
-
-using LearnBase
 import LearnBase: learn!, update!
 
 export
-    LearningStrategy, MetaLearner, make_learner, LearnType,
+    LearningStrategy, MetaStrategy, strategy, LearnType,
     # LearningStrategies
-    MaxIter, TimeLimit, ConvergenceFunction, IterFunction, ShowStatus, Tracer,
+    Verbose, MaxIter, TimeLimit, ConvergenceFunction, IterFunction, ShowStatus, Tracer,
     Converged, ConvergedTo,
     # functions
-    pre_hook, iter_hook, learn!, update!, post_hook, finished
+    setup!, update!, hook, finished, cleanup!, learn!
 
-# ----------------------------------------------------------------------
-
+#---------------------------------------------------------------------# LearningStrategy interface
 """
 A LearningStrategy should implement at least one of the following methods:
 
-- `pre_hook(strat, model)`
-- `post_hook(strat, model)`
-- `iter_hook(strat, model, i)`
-- `finished(strat, model, i)`
+- `setup!(strat, model, data)`
+- `cleanup!(strat, model)`
+- `hook(strat, model, i)`
+- `finished(strat, model, data, i)`
 - `update!(model, strat, item)`
 """
 abstract type LearningStrategy end
 
-# fallbacks don't do anything
-pre_hook(strat::LearningStrategy, model)      = return
-iter_hook(strat::LearningStrategy, model, i)  = return
-post_hook(strat::LearningStrategy, model)     = return
-finished(strat::LearningStrategy, model, i)   = false
-update!(model, strat::LearningStrategy, item) = return
+setup!(s::LearningStrategy, model) = nothing
+setup!(s::LearningStrategy, model, data) = setup!(s, model)
 
-# ----------------------------------------------------------------------
+update!(model, s::LearningStrategy, item) = nothing
 
-include("metalearner.jl")
-include("strategies.jl")
+hook(s::LearningStrategy, model, i) = nothing
+hook(s::LearningStrategy, model, data, i) = hook(s, model, i)
 
-# ----------------------------------------------------------------------
+finished(s::LearningStrategy, model, i) = false
+finished(s::LearningStrategy, model, data, i) = finished(s, model, i)
+
+cleanup!(s::LearningStrategy, model) = nothing
+
+
+
+#-----------------------------------------------------------------------# MetaStrategy
+"""
+    MetaStrategy(strats::LearningStrategy...)
+
+A collection of learning strategies in a type-stable way.
+"""
+struct MetaStrategy{T <: Tuple} <: LearningStrategy
+    strategies::T
+end
+MetaStrategy(s::LearningStrategy...) = MetaStrategy(s)
+
+function Base.show(io::IO, s::MetaStrategy)
+    println(io, "MetaStrategy")
+    println.(io, "  > ", s.strategies[1:(end-1)])
+    print(io, "  > ", s.strategies[end])
+end
+
+setup!(ms::MetaStrategy, model, data) = foreach(s -> setup!(s, model, data), ms.strategies)
+
+update!(model, s::MetaStrategy, item) = foreach(x -> update!(model, x, item), s.strategies)
+
+hook(s::MetaStrategy, model, data, i) = foreach(x -> hook(x, model, data, i), s.strategies)
+
+finished(s::MetaStrategy, model, data, i) = any(x -> finished(x, model, data, i),  s.strategies)
+
+cleanup!(s::MetaStrategy, model) = foreach(x -> cleanup!(x, model),    s.strategies)
+
+
+strategy(s::LearningStrategy...) = MetaStrategy(s)
+strategy(ms::MetaStrategy, s::LearningStrategy...) = MetaStrategy(ms.strategies..., s...)
+
+
+
+#-----------------------------------------------------------------------# LearnType
+module LearnType
+    struct Offline end
+    struct Online  end
+end
+
+#-----------------------------------------------------------------------# Online learn!
+function learn!(model, meta::LearningStrategy, data, ::LearnType.Online = LearnType.Online())
+    setup!(meta, model, data)
+    for (i, item) in enumerate(data)
+        update!(model, meta, item)
+        hook(meta, model, i)
+        finished(meta, model, data, i) && break
+    end
+    cleanup!(meta, model)
+end
+
+#-----------------------------------------------------------------------# Offline learn!
+function learn!(model, meta::LearningStrategy, data, ::LearnType.Offline)
+    setup!(meta, model, data)
+    i = 1
+    while true
+        update!(model, meta, data)
+        hook(meta, model, i)
+        finished(meta, model, data, i) && break
+        i += 1
+    end
+    cleanup!(meta, model)
+end
+
+#-----------------------------------------------------------------------# InfiniteNothing
+# learn without input data... good for minimizing functions
+struct InfiniteNothing end
+Base.start(itr::InfiniteNothing) = 1
+Base.done(itr::InfiniteNothing, i) = false
+Base.next(itr::InfiniteNothing, i) = (nothing, i + 1)
+
+learn!(model, meta::LearningStrategy) = learn!(model, meta, InfiniteNothing())
+
+
+
+
+#-----------------------------------------------------------------------#
+#                           Below here are built-in LearningStrategies
+#-----------------------------------------------------------------------#
+
+#-----------------------------------------------------------------------# Verbose
+"""
+    Verbose(s::LearningStrategy)
+
+Allow the LearningStrategy `s` to print output.
+"""
+struct Verbose{S <: LearningStrategy} <: LearningStrategy
+    strategy::S
+end
+
+Base.show(io::IO, v::Verbose) = print(io, "Verbose ", v.strategy)
+
+function finished(v::Verbose, model, data, i)
+    done = finished(v.strategy, model, data, i)
+    done && info("$(v.strategy) finished")
+    done
+end
+
+
+#-----------------------------------------------------------------------# MaxIter
+"""
+    MaxIter(n)
+
+Stop learning after `n` iterations.
+"""
+struct MaxIter <: LearningStrategy
+    n::Int
+end
+MaxIter() = MaxIter(100)
+Base.show(io::IO, s::MaxIter) = print(io, "MaxIter($(s.n))")
+finished(s::MaxIter, model, data, i) = i >= s.n
+
+#-----------------------------------------------------------------------# TimeLimit
+"""
+    TimeLimit(s)
+
+Stop learning after `s` seconds.
+"""
+mutable struct TimeLimit <: LearningStrategy
+    secs::Float64
+    secs_end::Float64
+    TimeLimit(secs::Number) = new(secs)
+end
+Base.show(io::IO, s::TimeLimit) = print(io, "TimeLimit($(s.secs))")
+setup!(strat::TimeLimit, model, data) = (strat.secs_end = time() + strat.secs)
+finished(strat::TimeLimit, model, data, i) = time() >= strat.secs_end
+
+
+#-----------------------------------------------------------------------# Converged
+"""
+    Converged(f; tol = 1e-6, every = 1)
+
+Stop learning when `norm(f(model) - lastf) ≦ tol`.
+"""
+mutable struct Converged{F <: Function} <: LearningStrategy
+    f::F          # f(model)
+    tol::Float64  # normdiff tolerance
+    every::Int    # only check every ith iteration
+    lastval::Vector{Float64}
+end
+Converged(f::Function; tol::Number = 1e-6, every::Int = 1) = Converged(f, tol, every, zeros(0))
+
+setup!(s::Converged, model, data) = (s.lastval = zeros(s.f(model)); return)
+function finished(strat::Converged, model, data, i)
+    val = strat.f(model)
+    if norm(val - strat.lastval) <= strat.tol
+        true
+    else
+        copy!(strat.lastval, val)
+        false
+    end
+end
+function finished(v::Verbose{<:Converged}, model, data, i)
+    done = finished(v.strategy, model, data, i)
+    done && info("Converged after $i iterations: $(v.strategy.f(model))")
+    done
+end
+
+
+
+
+
+#-----------------------------------------------------------------------# ConvergenceFunction
+"""
+    ConvergenceFunction(f)
+Stop learning when `f(model, i)` returns true.
+"""
+struct ConvergenceFunction <: LearningStrategy
+    f::Function
+end
+finished(strat::ConvergenceFunction, model, data, i)::Bool = strat.f(model, i)
+
+
+
+#-----------------------------------------------------------------------# ShowStatus
+"""
+    ShowStatus(b = 1)
+    ShowStatus(b, f)
+Every `b` iterations, print the output of `f(model, i)`.
+"""
+struct ShowStatus <: LearningStrategy
+    every::Int
+    f::Function
+end
+ShowStatus(every::Int = 1) = ShowStatus(every, (model, i) -> "Iteration $i: $(params(model))")
+setup!(strat::ShowStatus, model, data) = hook(strat, model, 0)
+function hook(strat::ShowStatus, model, i)
+    mod1(i, strat.every) == strat.every && println(strat.f(model, i))
+    return
+end
+
+
+
+
+
+#-----------------------------------------------------------------------# ConvergedTo
+"""
+    ConvergedTo(f, goal; tol=1e-6, every=1)
+Stop learning when `‖f(model) - goal‖ ≦ tol`.
+"""
+struct ConvergedTo{V} <: LearningStrategy
+    f::Function   # f(model)
+    tol::Float64  # normdiff tolerance
+    goal::V       # goal value
+    every::Int    # only check every ith iteration
+end
+function ConvergedTo(f::Function, goal; tol::Number = 1e-6, every::Int = 1)
+    ConvergedTo(f, tol, goal, every)
+end
+function finished(strat::ConvergedTo, model, data, i)
+    val = strat.f(model)
+    if norm(val - strat.goal) <= strat.tol
+        info("Converged after $i iterations: $val")
+        true
+    else
+        false
+    end
+end
+
+#-----------------------------------------------------------------------# IterFunction
+"""
+    IterFunction(f, b=1)
+Call `f(model, i)` every `b` iterations.
+"""
+struct IterFunction <: LearningStrategy
+    f::Function
+    every::Int
+end
+IterFunction(f::Function; every::Int = 1) = IterFunction(f, every)
+function hook(strat::IterFunction, model, i)
+    if mod1(i, strat.every) == strat.every
+        strat.f(model, i)
+    end
+    return
+end
+
+#-----------------------------------------------------------------------# Tracer
+"""
+    Tracer{T}(::Type{T}, f, b=1)
+Store `f(model, i)` every `b` iterations.
+"""
+struct Tracer{S} <: LearningStrategy
+    every::Int
+    f::Function
+    storage::Vector{S}
+end
+Tracer{S}(::Type{S}, f::Function, every::Int = 1) = Tracer(every, f, S[])
+function hook(strat::Tracer, model, i)
+    if mod1(i, strat.every) == strat.every
+        push!(strat.storage, strat.f(model, i))
+    end
+    return
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,20 @@
 module LearningStrategiesTest
-using LearningStrategies
-using Base.Test
 
+using LearningStrategies, Base.Test
+import LearningStrategies: update!
 
+mutable struct Model n::Int end
+struct NewStrat <: LearningStrategy end
+update!(m::Model, s::NewStrat, item) = (m.n += 1)
 
+#-----------------------------------------------------------------------# Verbose
+info("Verbose")
+learn!(nothing, Verbose(MaxIter(5)), 1:10)
+learn!(nothing, Verbose(TimeLimit(.1)))
+learn!(Model(5), Verbose(Converged(m -> m.n == 5)))
 
+#-----------------------------------------------------------------------# Type stability
+info("Type Stability")
 strat_list = [
     MaxIter(20),
     TimeLimit(2),
@@ -15,76 +25,86 @@ strat_list = [
     IterFunction((m, i) -> println("this is iteration $i")),
     Tracer(Float64, (m, i) -> mean(m))
 ]
-@testset "Type stability" begin
-    m = ones(2)
-    i = 5
-    for s in strat_list
-        println("  > ", s)
-        # println("    - ", "pre_hook")
-        @inferred pre_hook(s, m)
-        # println("    - ", "post_hook")
-        @inferred post_hook(s, m)
-        # println("    - ", "iter_hook")
-        @inferred iter_hook(s, m, i)
-        # println("    - ", "finished")
-        @inferred finished(s, m, i)
-        # println("    - ", "update!")
-        @inferred update!(m, s, i)
+m = ones(2)
+i = 5
+for s in strat_list
+    println("  > ", s)
+    @inferred setup!(s, m, nothing)
+    @inferred cleanup!(s, m)
+    @inferred hook(s, m, i)
+    @inferred finished(s, m, nothing, i)
+    @inferred update!(m, s, i)
+end
+
+#-----------------------------------------------------------------------# "Real" Tests
+@testset "MaxIter" begin
+    for j in 1:10
+        m = Model(0)
+        learn!(m, strategy(MaxIter(j), NewStrat()), 1:100)
+        @test m.n == j
     end
 end
-
-@testset "MaxIter/IterFunction" begin
-    model = [0]
-    s = make_learner(MaxIter(20), IterFunction((m,i) -> (m[1] += 1)))
-    @inferred learn!(model, s)
-    @test model[1] == 20
-end
-
 @testset "TimeLimit" begin
-    model = nothing
-    s = make_learner(TimeLimit(2))
     t1 = time()
-    @inferred learn!(model, s)
+    learn!(nothing, TimeLimit(.5))
     elapsed = time() - t1
-    @test elapsed < 3
+    @test .5 <= elapsed < 1
 end
 
-@testset "ShowStatus" begin
-    model = nothing
-    s = make_learner(MaxIter(2), ShowStatus(1, (m, i) -> "    model is still $model"))
-    @inferred learn!(model, s)
-end
 
-@testset "ConvergenceFunction" begin
-    model = nothing
-    s = make_learner(ConvergenceFunction((m,i) -> true))
-    @inferred learn!(model, s)
-end
+#
+# @testset "MaxIter/IterFunction" begin
+#     model = [0]
+#     s = strategy(MaxIter(20), IterFunction((m,i) -> (m[1] += 1)))
+#     @inferred learn!(model, s)
+#     @test model[1] == 20
+# end
+#
+# @testset "TimeLimit" begin
+#     model = nothing
+#     s = strategy(TimeLimit(2))
+#     t1 = time()
+#     @inferred learn!(model, s)
+#     elapsed = time() - t1
+#     @test elapsed < 3
+# end
 
-@testset "Converged" begin
-    model = ones(5)
-    s = make_learner(Converged(m -> m))
-    @inferred learn!(model, s)
-end
-
-@testset "ConvergedTo" begin
-    model = ones(5)
-    s = make_learner(ConvergedTo(m -> m, ones(5)))
-    @inferred learn!(model, s)
-end
-
-@testset "IterFunction" begin
-    model = ones(5)
-    s = make_learner(MaxIter(2), IterFunction((m,i) -> println("    print 2 times!")))
-    @inferred learn!(model, s)
-end
-
-@testset "Tracer" begin
-    model = 1.0
-    s = make_learner(MaxIter(3), Tracer(Float64, (m,i) -> m))
-    @inferred learn!(model, s)
-    @test s.managers[2].storage == ones(3)
-end
+# @testset "ShowStatus" begin
+#     model = nothing
+#     s = make_learner(MaxIter(2), ShowStatus(1, (m, i) -> "    model is still $model"))
+#     @inferred learn!(model, s)
+# end
+#
+# @testset "ConvergenceFunction" begin
+#     model = nothing
+#     s = make_learner(ConvergenceFunction((m,i) -> true))
+#     @inferred learn!(model, s)
+# end
+#
+# @testset "Converged" begin
+#     model = ones(5)
+#     s = make_learner(Converged(m -> m))
+#     @inferred learn!(model, s)
+# end
+#
+# @testset "ConvergedTo" begin
+#     model = ones(5)
+#     s = make_learner(ConvergedTo(m -> m, ones(5)))
+#     @inferred learn!(model, s)
+# end
+#
+# @testset "IterFunction" begin
+#     model = ones(5)
+#     s = make_learner(MaxIter(2), IterFunction((m,i) -> println("    print 2 times!")))
+#     @inferred learn!(model, s)
+# end
+#
+# @testset "Tracer" begin
+#     model = 1.0
+#     s = make_learner(MaxIter(3), Tracer(Float64, (m,i) -> m))
+#     @inferred learn!(model, s)
+#     @test s.managers[2].storage == ones(3)
+# end
 
 
 
