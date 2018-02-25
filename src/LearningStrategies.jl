f29cbaefe1c8cbbfef2265108ac4a61a4be2c5b9
Iblis Lin <iblis@hs.ntnu.edu.tw>
2018-02-25 16:20:57 +0800

    update docstring of IterFunction

--- src/LearningStrategies.jl
+++ src/LearningStrategies.jl
@@ -250,10 +250,12 @@ end
 
 #-----------------------------------------------------------------------# IterFunction
 """
+    IterFunction(f)
     IterFunction(f, b)
     IterFunction(b, f)
 
-Call `f(model, i)` every `b` iterations.
+Call `f(model, i)` every `b` iterations at `hook` call.
+Default value of `b` is 1.
 """
 struct IterFunction{F<:Function} <: LearningStrategy
     f::F
